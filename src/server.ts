85af6e1bf7ea34dda33c4b678da229626a4e1b83
MyStic <khanh.votuan@hcmut.edu.vn>
2024-03-26 22:38:21 +0700

    config mongodb, dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
 import app from './app';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const DB =
+  process.env.DATABASE?.replace(
+    '<password>',
+    process.env.DATABASE_PASSWORD || ''
+  ) || '';
+
+mongoose.connect(DB).then(() => {
+  console.log('Successfully ');
+});
 
 const PORT = 8080;
 app.listen(PORT, () => {
-  console.log('Hello from Mystic server123');
+  console.log('Hello from Mystic server');
 });
