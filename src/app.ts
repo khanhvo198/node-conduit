261164dda08e0bb2ec7cbd74b7f33ef078293c38
MyStic <khanh.votuan@hcmut.edu.vn>
2024-04-03 09:51:20 +0700

    register error handler, refactor app using services

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
 import express from 'express';
 import { authRoutes } from './routes/authRoutes';
 import { userRoutes } from './routes/userRoutes';
+import HttpException from './utils/http-exception.model';
+import { Request, Response, NextFunction } from 'express';
+import routes from './routes/routes';
 
 const app = express();
 
 app.use(express.json());
-app.use('/api', authRoutes);
-app.use('/api', userRoutes);
+
+app.use('/api', routes);
+
+app.use(
+  (
+    err: Error | HttpException,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    // @ts-ignore
+    if (err && err.errorCode) {
+      // @ts-ignore
+      res.status(err.errorCode).json(err.message);
+    }
+  }
+);
 
 export default app;
